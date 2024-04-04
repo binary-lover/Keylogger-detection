0e49d1457d9deb9f5f3439b5748c937e341549fe
binarylover <whitedevil.hamd@gmail.com>
2024-04-04 23:00:18 +0530

    first commit

---
+++ Detection.cpp
@@ -0,0 +1,60 @@
+// keylogger detection application using C++ and WinAPI
+
+#include <iostream>
+#include <windows.h>
+#include <tlhelp32.h>
+#include <tchar.h>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+void isKeyloggerRunning() {
+    HANDLE hProcessSnap;
+    PROCESSENTRY32 pe32;
+
+    // Take a snapshot of all processes in the system.
+    hProcessSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+    if (hProcessSnap == INVALID_HANDLE_VALUE) {
+        std::cerr << "CreateToolhelp32Snapshot failed." << std::endl;
+        return;
+    }
+
+    // Set the size of the structure before using it.
+    pe32.dwSize = sizeof(PROCESSENTRY32);
+
+    // Retrieve information about the first process, and exit if unsuccessful.
+    if (!Process32First(hProcessSnap, &pe32)) {
+        std::cerr << "Process32First failed." << std::endl;
+        CloseHandle(hProcessSnap);
+        return;
+    }
+
+    cout<<"[+] Scanning running processes...\n";
+    bool keyloggerDitected = 0;   
+    do {
+        std::string processName = pe32.szExeFile;
+        // cout << "Process Name: " << processName << endl;
+        if (processName.find("keylog") != std::string::npos || processName.find("key_log") != std::string::npos || processName.find("k_log") != std::string::npos || processName.find("logger") != std::string::npos )
+        {
+            keyloggerDitected = 1;
+            cout << "[!] Keylogger detected..! Process Name: " << processName <<" Process ID: "<<pe32.th32ProcessID<<endl;
+            break;
+        }
+    } while (Process32Next(hProcessSnap, &pe32));
+
+    if (!keyloggerDitected)
+    {
+        cout << "[-] No Keylogger named process running"<<endl;
+    }
+    // clean up the snapshot object.
+    CloseHandle(hProcessSnap);
+}
+
+int main() {
+    isKeyloggerRunning();
+
+    return 0;
+
+}
---
+++ keylogger2343.cpp
@@ -0,0 +1,13 @@
+// pause program for  2 minuts using sleep function
+
+#include <iostream>
+#include <windows.h>
+
+using namespace std;
+
+int main() {
+    // Pause the program for 2 minutes
+    cout<<"Keylogger is running\n";
+    Sleep(520000);
+    return 0;
+}
